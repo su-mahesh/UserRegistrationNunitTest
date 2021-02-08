1218ba0f0a859e5fda3cfe6541fd0bffc8ae71d9
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-08 13:30:00 +0530

    [MaheshKangude] ADDED: LastName Validation NunitTestCases

--- UserRegistration/UserRegistrationRegex.cs
+++ UserRegistration/UserRegistrationRegex.cs
@@ -6,11 +6,15 @@ namespace UserRegistrationNameSpace
     public class UserRegistrationRegex
     {
         public Regex FirstNameRegex = new Regex(@"^[A-Z][a-z]{2,}$");
+        public Regex LastNameRegex = new Regex(@"^[A-Z][a-zA-Z]{2,}$");
 
         public bool ValidateFirstName(string FirstName)
         {
             return FirstNameRegex.IsMatch(FirstName);
         }
-
+        public bool ValidateLastName(string LastName)
+        {
+            return LastNameRegex.IsMatch(LastName);
+        }
     }
 }
--- UserRegistrationNUnit/UserRegistrationUnitTest.cs
+++ UserRegistrationNUnit/UserRegistrationUnitTest.cs
@@ -24,5 +24,19 @@ namespace UserRegistrationNUnit
             bool result = userRegistration.ValidateFirstName("MAHESH");
             Assert.IsFalse(result);
         }
+
+        [Test]
+        public void GivenLastName_WhenValid_ShouldReturnTrue()
+        {
+            bool result = userRegistration.ValidateLastName("Kangude");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void GivenLastName_WhenInvalid_ShouldReturnFalse()
+        {
+            bool result = userRegistration.ValidateLastName("kangude");
+            Assert.IsFalse(result);
+        }
     }
 }
